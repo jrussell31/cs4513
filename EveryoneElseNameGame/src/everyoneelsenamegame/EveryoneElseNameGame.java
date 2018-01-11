0550718aa535a63bf21019f5f4f074e02e2f4b82
Jon Lowrey <jon@jonlowrey.com>
2018-01-10 21:01:24 -0600

    Upload Program
    Jon Lowrey

---
+++ EveryoneElseNameGame/src/everyoneelsenamegame/EveryoneElseNameGame.java
@@ -0,0 +1,23 @@
+package everyoneelsenamegame;
+import java.lang.String;
+
+/**
+ *
+ * @author Jon
+ */
+public class EveryoneElseNameGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String theNames[] = {"Jon Lowrey"};
+        
+        
+        for(String S : theNames){
+           System.out.println(S);
+        
+        }
+    }
+    
+}
