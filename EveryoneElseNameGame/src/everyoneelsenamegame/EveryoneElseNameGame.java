7b27412d2fc13204c593d77eefdf8159f5e534ed
Msfile <mfile@uco.edu>
2018-01-11 20:32:24 -0600

    Added my name "Michael File" to the program. (#3)

--- EveryoneElseNameGame/src/everyoneelsenamegame/EveryoneElseNameGame.java
+++ EveryoneElseNameGame/src/everyoneelsenamegame/EveryoneElseNameGame.java
@@ -11,7 +11,7 @@ public class EveryoneElseNameGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String theNames[] = {"Jon Lowrey","Mary Ozor", "Josh Aynes"};
+        String theNames[] = {"Jon Lowrey","Mary Ozor", "Josh Aynes", "Michael File"};
         
         
         for(String S : theNames){
