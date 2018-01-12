8662fe506a34e44b1f340e6abbe76e12fefb3b2c
Cameron Llewellyn <cameron.b.llewellyn@gmail.com>
2018-01-12 08:24:01 -0600

    Added my name to test (#4)
    Testing GitHub desktop

--- EveryoneElseNameGame/src/everyoneelsenamegame/EveryoneElseNameGame.java
+++ EveryoneElseNameGame/src/everyoneelsenamegame/EveryoneElseNameGame.java
@@ -11,7 +11,7 @@ public class EveryoneElseNameGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String theNames[] = {"Jon Lowrey","Mary Ozor", "Josh Aynes", "Michael File"};
+        String theNames[] = {"Jon Lowrey","Mary Ozor", "Josh Aynes", "Michael File", "Cameron Llewellyn"};
         
         
         for(String S : theNames){
