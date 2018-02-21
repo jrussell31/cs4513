bcb20fc8d411f724ad9be78ced79861671896488
Unknown <mfile@uco.edu>
2018-02-20 22:20:10 -0600

    Updated Fireball to reflect Direction class

--- DungeonCrawl/src/model/Moveable/Fireball.java
+++ DungeonCrawl/src/model/Moveable/Fireball.java
@@ -12,6 +12,7 @@ import controller.ObjectAnimator;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import model.Direction;
 import model.GameData;
 import model.GameObject;
 import model.Immoveable.Tile.Fire;
@@ -19,198 +20,158 @@ import model.Immoveable.Tile.Wall;
 import model.Immoveable.Tile.Water;
 import model.Level;
 
-
 /**
  *
  * @author russe_000
  */
 public class Fireball extends Monster {
-    
+
     public float dx;
     public float dy;
-    
+
     private int counter = 0;
-    
+
     public BufferedImage[] fireballSprites;
-    
-    public boolean turnRight, turnUp, turnDown = false; 
-    public boolean turnLeft = true; 
-    
+
     private final ObjectAnimator fireballMoves;
-    
+
     public Fireball(float x, float y) {
         super(x, y);
         fireballMoves = new ObjectAnimator();
-        
+        super.direction = Direction.LEFT;
+
         fireballSprites = new BufferedImage[1];
-        try{
-            BufferedImage image = (BufferedImage)ImageFinder.getImage("ImagesFolder", "Fireball.png");           
-            fireballSprites[0] = image;                       
-        } catch(Exception e){
+        try {
+            BufferedImage image = (BufferedImage) ImageFinder.getImage("ImagesFolder", "Fireball.png");
+            fireballSprites[0] = image;
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }
-    
+
     @Override
     public void render(Graphics2D g) {
-        g.drawImage(fireballMoves.getImage(), (int)super.x, (int)super.y, (int)super.WIDTH, (int)super.HEIGHT, 
-            null);
-        
+        g.drawImage(fireballMoves.getImage(), (int) super.x, (int) super.y, (int) super.WIDTH, (int) super.HEIGHT,
+                null);
+
         //Draw Collision Box
         g.setColor(Color.blue);
         g.draw(this.getCollisionBox());
     }
-       
+
     @Override
     public void update() {
         dx = super.x;
         dy = super.y;
-        fireballMoves.setFrames(fireballSprites);        
-        if (Level.fLevelOne){            
-            if (turnLeft){
-                if (gameData.time > 0){
-                    if (counter == 1000){
-                        counter = 0;                         
-                        super.x -= super.MOVEMENT;                     
-                        if (super.x <= 64) {
-                            fireballMoves.setFrames(fireballSprites);
-                            turnLeft = false; 
-                            turnRight = false; 
-                            turnUp = false; 
-                            turnDown = true;
-                        }                      
+        fireballMoves.setFrames(fireballSprites);
+        if (Level.fLevelOne) {
+            switch (direction) {
+                case LEFT:
+                    if (gameData.time > 0) {
+                        if (counter == 1000) {
+                            counter = 0;
+                            super.x -= super.MOVEMENT;
+                            if (super.x <= 64) {
+                                fireballMoves.setFrames(fireballSprites);
+                                direction = Direction.DOWN;
+                            }
+                        } else {
+                            counter += 100;
+                        }
                     }
-                    else{
-                        counter += 100; 
+                    break;
+                case DOWN:
+                    if (gameData.time > 0) {
+                        if (counter == 1000) {
+                            counter = 0;
+                            super.y += super.MOVEMENT;
+                            if (super.y >= 192) {
+                                fireballMoves.setFrames(fireballSprites);
+                                direction = Direction.RIGHT;
+                            }
+                        } else {
+                            counter += 100;
+                        }
                     }
-                } 
+                    break;
+                case RIGHT:
+                    if (gameData.time > 0) {
+                        if (counter == 1000) {
+                            counter = 0;
+                            super.x += super.MOVEMENT;
+                            if (super.x >= 192) {
+                                fireballMoves.setFrames(fireballSprites);
+                                direction = Direction.UP;
+                            }
+                        } else {
+                            counter += 100;
+                        }
+                    }
+                    break;
+                case UP:
+                    if (gameData.time > 0) {
+                        if (counter == 1000) {
+                            counter = 0;
+                            super.y -= super.MOVEMENT;
+                            if (super.y <= 64) {
+                                fireballMoves.setFrames(fireballSprites);
+                                direction = Direction.LEFT;
+                            }
+                        } else {
+                            counter += 100;
+                        }
+                    }
+                    break;
             }
-           else if(turnDown){
-                if(gameData.time > 0){
-                    if(counter == 1000){
-                        counter = 0;                         
-                        super.y += super.MOVEMENT;                        
-                        if (super.y >= 192) {
-                            fireballMoves.setFrames(fireballSprites);
-                            turnLeft = false; 
-                            turnRight = true; 
-                            turnUp = false; 
-                            turnDown = false;
-                        }
-                    }
-                    else{
-                        counter += 100;                         
-                    }
-                }   
-            }
-            else if(turnRight){
-                if(gameData.time > 0){                    
-                    if(counter == 1000){
-                        counter = 0;                         
-                        super.x += super.MOVEMENT;                         
-                       if (super.x >= 192) {
-                            fireballMoves.setFrames(fireballSprites);
-                            turnLeft = false; 
-                            turnRight = false; 
-                            turnUp = true; 
-                            turnDown = false;
-                        }
-                    }
-                    else{
-                        counter += 100; 
-                    }
-                }                    
-            } 
-            else if(turnUp){
-                if(gameData.time > 0){                    
-                    if(counter == 1000){
-                        counter = 0;                         
-                        super.y -= super.MOVEMENT;                               
-                        if (super.y <= 64) {
-                            fireballMoves.setFrames(fireballSprites);
-                            turnLeft = true; 
-                            turnRight = false; 
-                            turnUp = false; 
-                            turnDown = false;
-                        }
-                    }
-                    else{
-                        counter += 100; 
-                    }
-                }               
-            } 
             fireballMoves.update();
+        } else if (Level.fLevelTwo) {
+            switch (direction) {
+                case LEFT:
+                    if (gameData.time > 0) {
+                        if (counter == 1000) {
+                            counter = 0;
+                            super.x -= super.MOVEMENT;
+                            fireballMoves.setFrames(fireballSprites);
+                        } else {
+                            counter += 100;
+                        }
+                    }
+                    break;
+                case RIGHT:
+                    if (gameData.time > 0) {
+                        if (counter == 1000) {
+                            counter = 0;
+                            super.x += super.MOVEMENT;
+                            fireballMoves.setFrames(fireballSprites);
+                        } else {
+                            counter += 100;
+                        }
+                    }
+                    break;
+            }
         }
-        else if (Level.fLevelTwo){
-            if (turnLeft){
-                if (gameData.time > 0){
-                    if (counter == 1000){
-                        counter = 0; 
-                        super.x -= super.MOVEMENT;                                                
-                        fireballMoves.setFrames(fireballSprites);                            
-                    }                      
-                    else{
-                        counter += 100; 
-                    }
-                } 
-            }
-            else if(turnRight){
-                if(gameData.time > 0){                    
-                    if(counter == 1000){
-                        counter = 0;                         
-                        super.x += super.MOVEMENT;                                               
-                        fireballMoves.setFrames(fireballSprites);                                                   
-                    }
-                    else{
-                        counter += 100; 
-                    }
-                }                    
-            }
-        }    
-        else{
-            
-        } 
     }
-    
+
     public void noMove() {
         super.x = dx;
         super.y = dy;
     }
-    
+
     @Override
-    public void collide(GameObject O){           
-        if(O instanceof Gamer){ 
-           this.setAlive(false);
-           GameData.gamer.setAlive(false);
-           //GameData.gamer.isDisplayed = false;
-           //this.isDisplayed = false;            
-           GameData.killedMonsters.add(this);
-           GameData.killedMonsters.add(GameData.gamer);
-           DungeonCrawl.bannerPanel.setBannerText("You collided with the Fireball on Level  " + GameData.currentLevel.getLevelValue());
-           GameData.levelInProgress = false;        
-        } 
-        else if(O instanceof Wall || O instanceof Fire || O instanceof Water){
-           if(turnLeft){
-              noMove(); 
-              turnLeft = false; 
-              turnRight = true; 
-           }
-           else if (turnRight){
-              noMove(); 
-              turnRight = false; 
-              turnLeft = true; 
-           }
-           else if (turnUp){
-               noMove(); 
-               turnUp = false; 
-               turnDown = true; 
-           }
-           else{
-               noMove(); 
-               turnDown = false; 
-               turnUp = true; 
-           }
+    public void collide(GameObject O) {
+        if (O instanceof Gamer) {
+            this.setAlive(false);
+            GameData.gamer.setAlive(false);
+            //GameData.gamer.isDisplayed = false;
+            //this.isDisplayed = false;            
+            GameData.killedMonsters.add(this);
+            GameData.killedMonsters.add(GameData.gamer);
+            DungeonCrawl.bannerPanel.setBannerText("You collided with the Fireball on Level  " + GameData.currentLevel.getLevelValue());
+            GameData.levelInProgress = false;
+        } else if (O instanceof Wall || O instanceof Fire || O instanceof Water || O instanceof Block) {
+            noMove();
+            direction = direction.getOppositeDirection();            
         }
-    }        
+    }
 }
