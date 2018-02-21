78168ca887a4f166e14828ea1d776bccd26fbb0f
Unknown <mfile@uco.edu>
2018-02-21 02:09:01 -0600

    Added new objects to LevelOne and moved some existing objects for testing purposes

--- DungeonCrawl/src/model/LevelOne.java
+++ DungeonCrawl/src/model/LevelOne.java
@@ -49,9 +49,9 @@ public final class LevelOne extends Level{
             super.immovableObjects.add(new Wall(MAP_WIDTH, i));
         }        
         
-        for(int i = 0; i <= 8; ++i){
+        for(int i = 0; i <= 13; ++i){
             super.immovableObjects.add(new Wall(i, 8));
-            if(i != 4){
+            if(i != 4 && i < 8){
                 super.immovableObjects.add(new Wall(8, i));
             }
         }
@@ -120,7 +120,7 @@ public final class LevelOne extends Level{
         
         //Level boots
         super.immovableObjects.add(new Boot(3, 3, BootType.FIRE));
-        super.immovableObjects.add(new Boot(3, 2, BootType.WATER));
+        super.immovableObjects.add(new Boot(15, 10, BootType.WATER));
         
         //Level Chips
         super.immovableObjects.add(new Chip(4, 3));
@@ -131,7 +131,11 @@ public final class LevelOne extends Level{
         
         //Level Elemental Walls
         super.immovableObjects.add(new Water(13,19));
+        super.immovableObjects.add(new Water(15, 8));
+        super.immovableObjects.add(new Water(15, 9));
+        super.immovableObjects.add(new Water(15, 12));
         super.immovableObjects.add(new Fire(17,19));
+        super.immovableObjects.add(new Fire(9, 4));
         
         //Toggle Walls
         ArrayList<GameObject> toggleWalls = new ArrayList<>();
@@ -148,9 +152,9 @@ public final class LevelOne extends Level{
         super.immovableObjects.add(new Portal(15, 26));
         
         //Floors
-        for(int i = 15; i < 25; ++i){
+        /*for(int i = 15; i < 25; ++i){
             super.immovableObjects.add(new Ice(i, 4));
-        }
+        }*/
     }
 
     @Override
@@ -166,8 +170,8 @@ public final class LevelOne extends Level{
         super.moveableObjects.add(new Ball(14,15));
         
         //Level blocks
-        super.moveableObjects.add(new Block(15, 3));
-        super.moveableObjects.add(new Block(17, 3));
+        super.moveableObjects.add(new Block(12, 6));
+        super.moveableObjects.add(new Block(13, 6));
     }
 
     @Override
