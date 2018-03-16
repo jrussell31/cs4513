144d53e16b276e100702461d8336b54cef891d9b
Unknown <mozor@uco.edu>
2018-03-16 07:56:38 -0500

    Auto stash before merge of "Dev" and "upstream/Dev"

--- DungeonCrawl/src/model/LevelOne.java
+++ DungeonCrawl/src/model/LevelOne.java
@@ -273,12 +273,11 @@ public final class LevelOne extends Level{
         super.moveableObjects.add(new Ball(14,15,Direction.DOWN));
         super.moveableObjects.add(new Ball(15,19,Direction.LEFT));
         super.moveableObjects.add(new Ball(13,4,Direction.RIGHT));
-
-        super.moveableObjects.add(new Glider(5,20,Direction.UP)); 
-        super.moveableObjects.add(new Glider(28,5,Direction.UP));
+       
+        super.moveableObjects.add(new Glider(17,7,Direction.UP));        
         //super.moveableObjects.add(new Bug(18, 19, Direction.RIGHT));
         super.moveableObjects.add(new Bug(12, 19, Direction.LEFT));
-        //super.moveableObjects.add(new Frog(1, 5));
+        super.moveableObjects.add(new Frog(1, 5));
                 
         //Tank
         ArrayList<GameObject> tanks = new ArrayList<>();
